27f2f38f80fb1b14b5378ef0c91f0776b8818484
klbxg <liweiwei0404@gmail.com>
2015-01-25 23:58:51 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,25 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Olivia Li on 1/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+
+
+@end
