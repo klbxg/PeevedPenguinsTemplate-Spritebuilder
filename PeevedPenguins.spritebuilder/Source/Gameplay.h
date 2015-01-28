b964b9925b1eb8a89f0b1f45c496ec1bf8cf536c
klbxg <liweiwei0404@gmail.com>
2015-01-27 19:14:36 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Olivia Li on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
