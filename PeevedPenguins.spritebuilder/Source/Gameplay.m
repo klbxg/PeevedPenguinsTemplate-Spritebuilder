ce831c1c13ded2248aef0a00b8664fb77eda12d0
klbxg <liweiwei0404@gmail.com>
2015-02-02 02:28:45 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -14,7 +14,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
-    CCNode *_pullbackNode;
+    //CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
 }
@@ -28,7 +28,7 @@
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
     // nothing shall collide with our invisible nodes
-    _pullbackNode.physicsBody.collisionMask = @[];
+    //_pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
