110de892b9ba933b30fcb8f30e4a2532cffe4089
klbxg <liweiwei0404@gmail.com>
2015-02-02 12:18:20 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -149,6 +149,14 @@
     [seal removeFromParent];
 }
 
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
 - (void)retry {
     // reload this level
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
